251cde74e3bdfe70776aac644ee8858515fbc55c
aktersnurra <gustaf.rydholm@gmail.com>
2021-02-07 20:26:10 +0100

    zsh fix for config and new tmux conf

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias zshconfig="nvim ~/.zshrc"
 alias gp='git add . && git commit -m "auto push" && git push'
 alias zrc='nvim ~/.zshrc'
 alias zsource='source ~/.zshrc'
-alias config='/usr/bin/git --git-dir=/Users/aktersnurra/dotfiles/ --work-tree=/Users/aktersnurra'
 alias v='nvim'
 alias vi='nvim'
 alias vim='nvim'
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
