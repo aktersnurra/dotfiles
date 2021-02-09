6453f0dbffd3d8efd04150d870b95c9cda34f57b
aktersnurra <grydholm@kth.se>
2021-02-09 22:06:04 +0100

    ls fix

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,7 +10,11 @@ alias vim='nvim'
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 
 if command -v exa > /dev/null; then
-  alias ls='exa -l'
+  alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+  alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+  alias ll='exa -l --color=always --group-directories-first'  # long format
+  alias lt='exa -aT --color=always --group-directories-first' # tree listing
+  alias l.='exa -a | egrep "^\."'
 else
   echo "install exa"
 fi
