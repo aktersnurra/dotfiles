2a262e9806e27920306aa8c89805e0e386adfe2c
aktersnurra <grydholm@kth.se>
2021-02-09 21:58:42 +0100

    Some cool rust dependencies

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -14,3 +14,34 @@ if command -v exa > /dev/null; then
 else
   echo "install exa"
 fi
+
+if command -v bat > /dev/null; then
+  alias cat='bat'
+else
+  echo "install bat"
+fi
+
+if command -v rg > /dev/null; then
+  alias grep='rg'
+else
+  echo "install ripgrep"
+fi
+
+if command -v fd > /dev/null; then
+  alias find='fd'
+else
+  echo "install fd"
+fi
+
+
+if ! command -v tokei > /dev/null; then
+  echo "install tokei"
+fi
+
+
+if command -v procs > /dev/null; then
+  alias ps='procs'
+else
+  echo "install procs"
+fi
+
