d4b9c144f24ddf4f22e32e33c05e0785bcff1beb
aktersnurra <grydholm@kth.se>
2021-02-10 19:42:38 +0100

    zsh autosuggestion

--- .config/zsh/antigen_pkgs.zsh
+++ .config/zsh/antigen_pkgs.zsh
@@ -6,6 +6,7 @@ antigen bundle pip
 antigen bundle command-not-found
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search ./zsh-history-substring-search.zsh
+antigen bundle zsh-users/zsh-autosuggestions
 
 # Done
 antigen apply
