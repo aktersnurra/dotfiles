8e97cff50b77a5c381447e675ea448865aa69d74
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2021-05-27 21:11:39 +0200

    Bug fix in PATH export, added some of Luke's scripts

---
+++ .config/zsh/.zshrc
@@ -0,0 +1,7 @@
+# ZSH config
+# Load plugins first
+[ -f $HOME/.config/zsh/plugins.zsh ] && source $HOME/.config/zsh/plugins.zsh
+[ -f $HOME/.config/zsh/exports.zsh ] && source $HOME/.config/zsh/exports.zsh
+[ -f $HOME/.config/zsh/functions.zsh ] && source $HOME/.config/zsh/functions.zsh
+[ -f $HOME/.config/zsh/keybindings.zsh ] && source $HOME/.config/zsh/keybindings.zsh
+[ -f $HOME/.config/zsh/aliases.zsh ] && source $HOME/.config/zsh/aliases.zsh
--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -15,7 +15,7 @@ function zle-keymap-select {
 
 # Prompt called purs
 function zle-line-init zle-keymap-select {
-  PROMPT=`$HOME/purs/target/release/purs prompt -k "$KEYMAP" -r "$?" --venv "${${VIRTUAL_ENV:t}%-*}"`
+  PROMPT=`$HOME/.local/src/purs/target/release/purs prompt -k "$KEYMAP" -r "$?" --venv "${${VIRTUAL_ENV:t}%-*}"`
   zle reset-prompt
 }
 zle -N zle-line-init
@@ -24,6 +24,6 @@ zle -N zle-keymap-select
 autoload -Uz add-zsh-hook
 
 function _prompt_purs_precmd() {
-  /$HOME/purs/target/release/purs precmd
+  /$HOME/.local/src/purs/target/release/purs precmd
 }
 add-zsh-hook precmd _prompt_purs_precmd
