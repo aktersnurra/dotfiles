443af22e38d99ec6c18e4af4816a053de2f1964c
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2026-03-16 22:34:58 +0100

    Update popup tmux

--- .config/tmux/popup.sh
+++ .config/tmux/popup.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-parent=$(tmux display-message -p "#{session_name}")
-popup="popup-$parent"
-
-if tmux has-session -t "$popup" 2>/dev/null; then
-    tmux attach-session -t "$popup"
-else
-    tmux new-session -s "$popup" \; \
-        rename-window "build" \; \
-        new-window -n "test" \; \
-        new-window -n "repl" \; \
-        select-window -t 1
-fi
+parent=$(tmux display-message -p '#S')
+tmux attach -t "popup-${parent}" 2>/dev/null || tmux new-session -s "popup-${parent}"
